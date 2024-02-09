5059f5e4dade00f3f2fd15f510374467a03e3377
anurag-rawat-cko <Anurag.rawat@checkout.com>
2024-02-09 19:22:19 +0530

    Working version of push notifs through firebase and one signal for android apk build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import OneSignal from 'onesignal-cordova-plugin';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    OneSignal.Debug.setLogLevel(6)
+    OneSignal.initialize(environment.OneSignalAppId);
+    OneSignal.Notifications.addEventListener('click', async (e) => {
+      let clickData = await e.notification;
+      console.log("Notification Clicked : " + clickData);
+    })
+    OneSignal.Notifications.requestPermission(true).then((success: Boolean) => {
+      console.log("Notification permission granted " + success);
+    })
+  }
 }
